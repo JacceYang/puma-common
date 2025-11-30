ded2853499ce7c9664b2f1efed5003c36f9b5d31
Your Name <your.email@example.com>
2025-11-30 15:14:39 +0800

    add publish mvn

--- puma-common-login/src/main/java/com/explorex/puma/common/login/LoginUser.java
+++ puma-common-login/src/main/java/com/explorex/puma/common/login/LoginUser.java
@@ -1,11 +1,9 @@
 package com.explorex.puma.common.login;
 
 import lombok.Builder;
-import lombok.Data;
 import java.io.Serializable;
 
 @Builder
-@Data
 public class LoginUser implements Serializable {
 
     private String token;
@@ -25,4 +23,77 @@ public class LoginUser implements Serializable {
     private String ipaddr;
 
     private  User user;
+
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getLoginLocation() {
+        return loginLocation;
+    }
+
+    public void setLoginLocation(String loginLocation) {
+        this.loginLocation = loginLocation;
+    }
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(String browser) {
+        this.browser = browser;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public long getExpireTime() {
+        return expireTime;
+    }
+
+    public void setExpireTime(long expireTime) {
+        this.expireTime = expireTime;
+    }
+
+    public long getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(long loginTime) {
+        this.loginTime = loginTime;
+    }
+
+    public String getIpaddr() {
+        return ipaddr;
+    }
+
+    public void setIpaddr(String ipaddr) {
+        this.ipaddr = ipaddr;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- puma-common-login/src/main/java/com/explorex/puma/common/login/User.java
+++ puma-common-login/src/main/java/com/explorex/puma/common/login/User.java
@@ -3,12 +3,37 @@ package com.explorex.puma.common.login;
 import lombok.Builder;
 import lombok.Data;
 
-@Data
+
 @Builder
 public class User {
     private Long uid;
     private String nickName;
     private long createTime;
+
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(long createTime) {
+        this.createTime = createTime;
+    }
 
     public User(Long uid, String nickName, long createTime) {
         this.nickName = nickName;
